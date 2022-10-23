4443d0d4930dde6f562043aa62b4a593f7262258
markwu372 <zwu372@gatech.edu>
2022-10-22 22:38:25 -0400

    flask added

---
+++ microservice/contacts_resource.py
@@ -0,0 +1,37 @@
+import pymysql
+
+import os
+
+
+class ContactsResource:
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def _get_connection():
+
+        usr = os.environ.get("DBUSER")
+        pw = os.environ.get("DBPW")
+        h = os.environ.get("DBHOST")
+
+        conn = pymysql.connect(
+            user=usr,
+            password=pw,
+            host=h,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
+
+    @staticmethod
+    def get_by_key(key):
+
+        sql = "SELECT * FROM Contacts_db.users where user_id=%s";
+        conn = ContactsResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=key)
+        result = cur.fetchone()
+
+        return result
+
