b00eef95b59f441f95574bc175ffb78fea5f0371
Jialin1210 <lynnzhu1210@gmail.com>
2022-12-15 20:20:50 -0500

    complete smarty api

--- microservice/app.py
+++ microservice/app.py
@@ -73,29 +73,6 @@ def get_contact_by_userid():
 
 @app.route('/contacts/create', methods=['GET', "POST"])
 def add_contact():
-    if request.method == "GET":
-        URL = "https://us-autocomplete-pro.api.smartystreets.com/lookup"
-        
-        auth_id = "4070fba4-036c-0f2d-aa09-b9ba9cceb8eb"
-        token = "XdzuVVnBsOovBpJSZej8"
-        search = request.form.get('address')
-        print('Smarty Search:', search)
-        if search:
-            # defining a params dict for the parameters to be sent to the API
-            PARAMS = {
-                'search':search,
-                'auth-id':auth_id,
-                'auth-token':token
-            }
-            
-            # sending get request and saving the response as response object
-            r = requests.get(url = URL, params = PARAMS)
-            
-            # extracting data in json format
-            data = r.json()
-            # print('Smarty Result: ', data)
-            return render_template('contacts.html', result=data)
-
     if request.method == 'POST':
         lastname = request.form.get('last_name')
         firstname = request.form.get('first_name')
